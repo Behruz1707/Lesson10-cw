a0dcf8caf015410596921e974d2cd8f387809420
Behruzbek <behruznosirjonov23@gmail.com>
2023-11-01 15:52:47 +0300

    Payment list added to user

--- src/model/Payment.java
+++ src/model/Payment.java
@@ -1,9 +1,12 @@
 package model;
 
+import java.util.List;
+
 public class Payment {
     private String accFrom;
     private String accTo;
     private double amount;
+    private List<Payment> payments;
 
     public Payment(String accFrom, String accTo, double amount) {
         this.accFrom = accFrom;
@@ -33,5 +36,13 @@ public class Payment {
 
     public void setAmount(double amount) {
         this.amount = amount;
+    }
+
+    public List<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(List<Payment> payments) {
+        this.payments = payments;
     }
 }
