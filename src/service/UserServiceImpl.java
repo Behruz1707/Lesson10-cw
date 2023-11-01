14a26c7d12466e31ad7e2e0a49ef99c313c52fb5
Behruzbek <behruznosirjonov23@gmail.com>
2023-11-01 16:16:24 +0300

    F3 saving logic changed

--- src/service/UserServiceImpl.java
+++ src/service/UserServiceImpl.java
@@ -3,6 +3,6 @@ package service;
 public class UserServiceImpl implements UserService{
     @Override
     public void save(String firstName, String lastName, double amount) {
-
+        //user saved
     }
 }
